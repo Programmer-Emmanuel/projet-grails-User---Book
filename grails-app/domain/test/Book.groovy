bde75d95dfd55a81489150af17ca487f6a313a00
👑Lord_Clxt👑 <127218888+LordClxt@users.noreply.github.com>
2026-06-12 16:59:08 +0000

    Add TODO comment for GORM mapping in Book class
    Added a TODO comment regarding GORM mapping for pages_number.

--- grails-app/domain/test/Book.groovy
+++ grails-app/domain/test/Book.groovy
@@ -3,6 +3,7 @@ package test
 class Book {
     Long id
     String title
+    // @TODO: Gorm fait automatiquement le mapping si tu as pages_number en db ici c'est pagesNumber
     Long pages_number
     static belongsTo = [user: User]
 
